795d8af8dfad6c9a624372e8f1dc2f4bbbc4f2b7
cyboravi <cyboravidell@gmail.com>
2025-02-16 16:01:00 +0530

    taking env variable from .env file

--- src/main/java/com/advocate/AdvocateApplication.java
+++ src/main/java/com/advocate/AdvocateApplication.java
@@ -5,20 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AdvocateApplication {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(AdvocateApplication.class);
 
-
 	public static void main(String[] args) {
-		
+
 		// Load environment variables from .env file
-//    	Dotenv dotenv = Dotenv.configure().load();
-//        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(AdvocateApplication.class, args);
 		logger.info("Advocate Application Started....");
